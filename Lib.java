59292712e395dbca7bb89c516d5f168e6935ee97
AlejoGG <alejo_95_8@hotmail.com>
2016-03-15 20:22:42 +0100

    modificaciones alumno 2

--- Lib.java
+++ Lib.java
@@ -11,6 +11,7 @@ public class Lib {
 
     public void metodoAlumno2(){
         metodo3();
+	System.out.println("modificado por alumno 2"); // alumno 2
     }
 
     public void metodo3(){
@@ -19,5 +20,7 @@ public class Lib {
 
     public void metodoComunitario(){
         System.out.println("cambio en master y en metodo3");
+	// modificaciones alumno 2
+	System.out.println("Alumno 2 inserta esta sentencia")
     }
 }
